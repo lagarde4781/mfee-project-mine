5cc5f653905bcd56d85eb2b75d51e231c77498dc
Alejandro Lagarde <c-alejandro.lagarde@charter.com>
2024-07-29 08:26:23 -0600

    token removed

--- apps/vue-app/src/app/api/capstoneApi.js
+++ apps/vue-app/src/app/api/capstoneApi.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const capstoneApi = axios.create({
   baseURL: 'https://test.neuraac.com/api',
   headers: {
-    Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImxhZ2FyZGUzQGdtYWlsLmNvbSIsImlhdCI6MTcyMjIzNTc4NSwiZXhwIjoxNzIyMjM5Mzg1fQ.H_MkKXKs5ZUt_tYI4yJHXi4seQixJjbVcjyWA1BbyPM`
+    Authorization: `Bearer`
   }
 });
 export default capstoneApi;
