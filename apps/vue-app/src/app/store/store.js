4ee521549087b7694371abd315a1d2a1a34eb8f7
Alejandro Lagarde <c-alejandro.lagarde@charter.com>
2024-07-29 00:10:11 -0600

    Session 6 ready

--- apps/vue-app/src/app/store/store.js
+++ apps/vue-app/src/app/store/store.js
@@ -8,5 +8,9 @@ export const store = reactive({
   posts: [],
   async getPosts() {
     this.posts = await getPosts();
+  },
+  postEditing: null,
+  setPostEditing(post) {
+    this.postEditing = post;
   }
 });
